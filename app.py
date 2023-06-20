46b88b486bace1e4f4b849429a5ff0c1be8d2042
SHAIK JAMEER BASHA <108229702+Jameers23@users.noreply.github.com>
2023-06-20 11:18:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -156,7 +156,7 @@ def calorie():
                 cal = 0.0
                 try:
                     for i in range(0,8):  
-                        cal += (quantity[i]*((calories[item[i]]))
+                        cal += (quantity[i]*((calories[item[i]])))
                 except:
                     flash('Enter the enter details correctly.')
                     return redirect(url_for('calorie'))
